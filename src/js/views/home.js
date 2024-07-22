672ad239cdc29e46696580a0503dcda22e1f956d
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-07-22 19:23:02 +0000

    added all basic styling

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,15 +3,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { Characters } from "../component/Characters";
 import { Planets } from "../component/Planets";
+import { Navbar } from "../component/navbar";
 
 export const Home = () => (
-	<div className="bg-dark">
-		<div className="p-5">
-			<h1 className="text-warning">Characters</h1>
-			<Characters/>
-			<p className="m-6">" "</p>
-			<h1 className="text-warning mt-20px">Planets</h1>
-			<Planets />
+	<>
+		<Navbar />
+		<div className="bg-dark">
+			<div className="p-5">
+				<h1 className="text-warning">Characters</h1>
+				<Characters/>
+				<p className="m-6">" "</p>
+				<h1 className="text-warning mt-20px">Planets</h1>
+				<Planets />
+			</div>
 		</div>
-	</div>
+	</>
 );
